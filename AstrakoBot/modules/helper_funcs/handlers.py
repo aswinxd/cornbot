760e8b4ce11b7af4e6aa1b019e78cc0cddf2bfff
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-04-11 15:57:28 +0530

    Update handlers.py

--- AstrakoBot/modules/helper_funcs/handlers.py
+++ AstrakoBot/modules/helper_funcs/handlers.py
@@ -7,7 +7,6 @@ from telegram.ext import CommandHandler, MessageHandler, Filters
 from pyrate_limiter import (
     BucketFullException,
     Duration,
-    RequestRate,
     Limiter,
     MemoryListBucket,
 )
